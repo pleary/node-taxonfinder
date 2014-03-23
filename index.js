b6d5e1a32c7f9340f3e03944e82e43ec2ce01b5f
Patrick Leary <patrick.r.leary@gmail.com>
2014-03-23 14:02:26 -0400

    Added a lot more of the logic

--- index.js
+++ index.js
@@ -7,129 +7,200 @@ console.log('Loading dictionaries...');
 dictionaries.load();
 console.log('Dictionaries are loaded');
 
+var isAbbreviatedGenus = function(workingName) {
+  return /^[A-Z][a-z]?$/.test(workingName);
+};
+
+var isAbbreviatedGenusWithPeriod = function(workingName) {
+  return /^[A-Z][a-z]?\.$/.test(workingName);
+};
+
+var startsWithPunctuation = function(word) {
+  return /^[^A-Za-z]*[\(\.\[;,]/.test(word);
+};
+
+var endsWithPunctuation = function(word) {
+  return /[,;\.\)\]][^A-Za-z]*$/i.test(word);
+};
+
 // var findAndMarkupNames = function(html) {
 //   var wordsWithOffsets = utility.explodeHtml(html);
 //   var lastGenus = null;
 //   var currentStateHash = null;
 //   wordsWithOffsets = utility.removeTagsFromElements(wordsWithOffsets);
-//   for(var i=0 ; i<wordsWithOffsets.length ; i++) {
+//   for (var i=0 ; i<wordsWithOffsets.length ; i++) {
 //     var word = wordsWithOffsets[i]['word'];
 //     currentStateHash = checkWordAgainstState(word, currentStateHash);
 //   }
 // };
 
 var checkWordAgainstState = function(word, currentStateHash) {
-  if(!word) word = '';
-  if(!currentStateHash) currentStateHash = {};
+  if (!word) word = '';
+  if (!currentStateHash) currentStateHash = {};
   word = word.trim();
   var cleanWord = utility.clean(word);
   var lowerCaseCleanWord = cleanWord.toLowerCase();
   var workingName = currentStateHash['workingName'];
   var workingRank = currentStateHash['workingRank'];
-  if(!workingName) workingName = '';
-
-  // Abbreviation
-  if (/^[A-Z][a-z]?\.$/.test(word)) {
-    return { workingName: cleanWord, workingRank: 'genus' };
+  currentStateHash['word'] = word;
+  currentStateHash['cleanWord'] = cleanWord;
+  currentStateHash['lowerCaseCleanWord'] = lowerCaseCleanWord;
+  if (!workingName) {
+    currentStateHash['workingName'] = '';
+    workingName = '';
   }
-  // Genus
-  if(isGenus(word, cleanWord, lowerCaseCleanWord)) {
-    if(/[,;\.\)\]][^A-Za-z]*$/i.test(word)) {
-      return { returnNames: [ utility.ucfirst(lowerCaseCleanWord) ] };
-    } else {
-      return { workingName: cleanWord, workingRank: 'genus' };
+
+  // Found Abbreviation
+  if (isAbbreviatedGenusWithPeriod(word)) {
+    returnHash = { workingName: cleanWord, workingRank: 'genus' };
+    if (workingName !== '') returnHash['returnNames'] = [ workingName ];
+    return returnHash;
+  }
+  // Within Genus
+  else if (workingRank === 'genus') {
+    // with valid species
+    if (isSpecies(currentStateHash)) {
+      if (endsWithPunctuation(word)) {
+        // that ends the name
+        return { returnNames: [ workingName + ' ' + cleanWord ] };
+      } else {
+        // that is the next word in a name
+        return { workingName: workingName + ' ' + cleanWord, workingRank: 'species' };
+      }
     }
   }
-  // Family or above
-  if(isFamilyOrAbove(word, cleanWord, lowerCaseCleanWord)) {
-    return { returnNames: [ utility.ucfirst(lowerCaseCleanWord) ] };
+  // Within Species
+  else if (workingRank === 'species') {
+    if (isSpecies(currentStateHash)) {
+      return { workingName: workingName + ' ' + cleanWord, workingRank: 'species' };
+    }
+    if (isRank(currentStateHash)) {
+      // node the use of `word` here to retain original punctuation
+      return { workingName: workingName + ' ' + word, workingRank: 'rank' };
+    }
+    if (isGenus(currentStateHash)) {
+      return { workingName: cleanWord, workingRank: 'genus', returnNames: [ workingName ] };
+    }
+    if (isFamilyOrAbove(currentStateHash)) {
+      return { returnNames: [ workingName, utility.ucfirst(lowerCaseCleanWord) ] };
+    }
+  }
+  // Within Rank
+  else if (workingRank === 'rank') {
+    if (isSpecies(currentStateHash)) {
+      return { workingName: workingName + ' ' + cleanWord, workingRank: 'species' };
+    }
+    if (isGenus(currentStateHash)) {
+      return { workingName: cleanWord, workingRank: 'genus', returnNames: [ workingName ] };
+    }
+    if (isFamilyOrAbove(currentStateHash)) {
+      return { returnNames: [ workingName, utility.ucfirst(lowerCaseCleanWord) ] };
+    }
+  }
+  // No working state
+  else {
+    if (isGenus(currentStateHash)) {
+      if (endsWithPunctuation(word)) {
+        return { returnNames: [ utility.ucfirst(lowerCaseCleanWord) ] };
+      } else {
+        return { workingName: cleanWord, workingRank: 'genus' };
+      }
+    }
+    if (isFamilyOrAbove(currentStateHash)) {
+      return { returnNames: [ utility.ucfirst(lowerCaseCleanWord) ] };
+    }
   }
   // Return the last known name
-  if(workingName) {
+  if (workingName) {
     return { returnNames: [ workingName ] };
   }
   return null;
 }
 
-var prepareReturnString = function(currentString) {
-  if(currentString.length <= 2) currentString = "";
-  if(!currentString) return null;
+var prepareReturnString = function(workingName) {
+  if (workingName.length <= 2) workingName = "";
+  if (!workingName) return null;
 
   // AMANITA MUSCARIA
-  if(match = currentString.match(/^([A-Z])([A-Z\[\]-]*)( |$)(.*$)/)) {
-    currentString = match[1] + match[2].toLowerCase() + match[3] + match[4];
+  if (match = workingName.match(/^([A-Z])([A-Z\[\]-]*)( |$)(.*$)/)) {
+    workingName = match[1] + match[2].toLowerCase() + match[3] + match[4];
   }
   // Amanita (MUSCARIA) MUSCARIA
-  if(match = currentString.match(/^(.+ \()([A-Z])([A-Z\[\]-]*)(\) |\)$)(.*$)/)) {
-    currentString = match[1] + match[2] + match[3].toLowerCase() + match[4] + match[5];
+  if (match = workingName.match(/^(.+ \()([A-Z])([A-Z\[\]-]*)(\) |\)$)(.*$)/)) {
+    workingName = match[1] + match[2] + match[3].toLowerCase() + match[4] + match[5];
   }
   // Amanita MUSCARIA MUSCARIA
   var nextString = null;
-  while(match = currentString.match(/^(.+ )([A-Z\[\]-]*)( |$)(.*$)/)) {
+  while(match = workingName.match(/^(.+ )([A-Z\[\]-]*)( |$)(.*$)/)) {
     nextString = match[1] + match[2].toLowerCase() + match[3] + match[4];
-    if(currentString == nextString) break;
-    else currentString = nextString;
+    if (workingName === nextString) break;
+    else workingName = nextString;
   }
   // Amanita sp.
-  if(match = currentString.match(/^(.*) ([^ ]+)$/))
+  if (match = workingName.match(/^(.*) ([^ ]+)$/))
   {
     var potentialRank = match[2];
-    if(dictionaries.hashes['ranks'][potentialRank]) {
-      currentString = match[1];
+    if (dictionaries.hashes['ranks'][potentialRank]) {
+      workingName = match[1];
     }
   }
-  return currentString;
+  return workingName;
 };
 
-var isSpecies = function(word, cleanWord, lowerCaseCleanWord, currentString) {
+var isSpecies = function(currentStateHash) {
+  utility.extract(currentStateHash, this);
   // (amanita)
-  if(/^[^A-Za-z]*[\(\.\[;,]/.test(word)) return null;
-  if(dictionaries.hashes['species_bad'][lowerCaseCleanWord]) return null;
-  if(/[0-9]/.test(cleanWord)) return null;
-  if(currentString.length > 2 && /^[A-Z-\(\)]+$/.test(currentString)) {
+  if (/^[^A-Za-z]*[\(\.\[;,]/.test(word)) return null;
+  if (dictionaries.hashes['species_bad'][lowerCaseCleanWord]) return null;
+  if (/[0-9]/.test(cleanWord)) return null;
+  if (workingName.length > 2 && /^[A-Z-\(\)]+$/.test(workingName)) {
     // AMANITA muscaria
-    if(/[a-z]/.test(cleanWord)) return null;
+    if (/[a-z]/.test(cleanWord)) return null;
   } else {
     // Amanita MUSCARIA
-    if(/[A-Z]/.test(cleanWord)) return null;
+    if (/[A-Z]/.test(cleanWord)) return null;
   }
-  if(dictionaries.hashes['species'][lowerCaseCleanWord] || dictionaries.hashes['species_new'][lowerCaseCleanWord]) {
+  if (dictionaries.hashes['species'][lowerCaseCleanWord] || dictionaries.hashes['species_new'][lowerCaseCleanWord]) {
     return 1;
   }
   return null;
 };
 
-var isNotGenusOrFamily = function(word, cleanWord, lowerCaseCleanWord) {
-  if(cleanWord.length <= 2) return true;
-  if(!(/^[A-Z][a-z-]+$/.test(cleanWord)) && !(/^[A-Z-]+$/.test(cleanWord))) return true;
-  if(dictionaries.hashes['overlap_new'][lowerCaseCleanWord]) return true;
+var isNotGenusOrFamily = function(currentStateHash) {
+  utility.extract(currentStateHash, this);
+  if (cleanWord.length <= 2) return true;
+  if (!(/^[A-Z][a-z-]+$/.test(cleanWord)) && !(/^[A-Z-]+$/.test(cleanWord))) return true;
+  if (dictionaries.hashes['overlap_new'][lowerCaseCleanWord]) return true;
   return false;
 };
 
-var isGenus = function(word, cleanWord, lowerCaseCleanWord) {
-  if(isNotGenusOrFamily(word, cleanWord, lowerCaseCleanWord)) return null;
-  if((dictionaries.hashes['genera'][lowerCaseCleanWord] || dictionaries.hashes['genera_new'][lowerCaseCleanWord]) &&
+var isGenus = function(currentStateHash) {
+  utility.extract(currentStateHash, this);
+  if (isNotGenusOrFamily(word, cleanWord, lowerCaseCleanWord)) return null;
+  if ((dictionaries.hashes['genera'][lowerCaseCleanWord] || dictionaries.hashes['genera_new'][lowerCaseCleanWord]) &&
      !dictionaries.hashes['genera_family'][lowerCaseCleanWord]) {
-    if(dictionaries.hashes['dict_ambig'][lowerCaseCleanWord] ) return 0.5;
+    if (dictionaries.hashes['dict_ambig'][lowerCaseCleanWord] ) return 0.5;
     else return 1;
   }
   return null;
 };
 
-var isFamilyOrAbove = function(word, cleanWord, lowerCaseCleanWord) {
-  if(isNotGenusOrFamily(word, cleanWord, lowerCaseCleanWord)) return null;
-  if(dictionaries.hashes['family'][lowerCaseCleanWord] || dictionaries.hashes['family_new'][lowerCaseCleanWord] ||
+var isFamilyOrAbove = function(currentStateHash) {
+  utility.extract(currentStateHash, this);
+  if (isNotGenusOrFamily(word, cleanWord, lowerCaseCleanWord)) return null;
+  if (dictionaries.hashes['family'][lowerCaseCleanWord] || dictionaries.hashes['family_new'][lowerCaseCleanWord] ||
      dictionaries.hashes['genera_family'][lowerCaseCleanWord]) {
-    if(dictionaries.hashes['dict_ambig'][lowerCaseCleanWord] ) return 0.5;
+    if (dictionaries.hashes['dict_ambig'][lowerCaseCleanWord] ) return 0.5;
     else return 1;
   }
   return null;
 };
 
-var isRank = function(word, cleanWord, lowerCaseCleanWord) {
-  if(/^[^A-Za-z]*[\(\.\[;,]/.test(word)) return null;
-  if(/[A-Z]/.test(cleanWord)) return null;
-  if(dictionaries.hashes['ranks'][lowerCaseCleanWord]) return 1;
+var isRank = function(currentStateHash) {
+  utility.extract(currentStateHash, this);
+  if (/^[^A-Za-z]*[\(\.\[;,]/.test(word)) return null;
+  if (/[A-Z]/.test(cleanWord)) return null;
+  if (dictionaries.hashes['ranks'][lowerCaseCleanWord]) return 1;
   return null;
 };
 
@@ -137,6 +208,10 @@ module.exports = {
   dictionaries: dictionaries,
   utility: utility,
   // findAndMarkupNames: findAndMarkupNames,
+  isAbbreviatedGenus: isAbbreviatedGenus,
+  isAbbreviatedGenusWithPeriod: isAbbreviatedGenusWithPeriod,
+  startsWithPunctuation: startsWithPunctuation,
+  endsWithPunctuation: endsWithPunctuation,
   prepareReturnString: prepareReturnString,
   checkWordAgainstState: checkWordAgainstState,
   isSpecies: isSpecies,
--- test/index.js
+++ test/index.js
@@ -1,6 +1,10 @@
 var expect = require('chai').expect,
     taxonfinder = require('../index'),
     // findAndMarkupNames = taxonfinder.findAndMarkupNames,
+    isAbbreviatedGenus = taxonfinder.isAbbreviatedGenus,
+    isAbbreviatedGenusWithPeriod = taxonfinder.isAbbreviatedGenusWithPeriod,
+    startsWithPunctuation = taxonfinder.startsWithPunctuation,
+    endsWithPunctuation = taxonfinder.endsWithPunctuation,
     checkWordAgainstState = taxonfinder.checkWordAgainstState,
     prepareReturnString = taxonfinder.prepareReturnString,
     isSpecies = taxonfinder.isSpecies,
@@ -9,40 +13,165 @@ var expect = require('chai').expect,
     isFamilyOrAbove = taxonfinder.isFamilyOrAbove,
     isRank = taxonfinder.isRank;
 
+var buildState = function(word, cleanWord, lowerCaseCleanWord, workingName) {
+  return { word: word, cleanWord: cleanWord, lowerCaseCleanWord: lowerCaseCleanWord,
+    workingName: workingName };
+};
+
 // describe('#findAndMarkupNames', function() {
 //   it('does something', function() {
-//     findAndMarkupNames("<p class='something'>Pomatomus saltator hello</p>");
+//     findAndMarkupNames("<p class='something'>Felis leo hello</p>");
 //   });
 // });
+
+describe('#isAbbreviatedGenus', function() {
+  it('recognizes abreviations without periods', function() {
+    expect(isAbbreviatedGenus("G")).to.be.true;
+    expect(isAbbreviatedGenus("Gr")).to.be.true;
+  });
+  it('recognizes non-abbreviations', function() {
+    expect(isAbbreviatedGenus("Gr.")).to.be.false;
+    expect(isAbbreviatedGenus("Gro")).to.be.false;
+  });
+});
+
+describe('#isAbbreviatedGenusWithPeriod', function() {
+  it('recognizes abreviations with periods', function() {
+    expect(isAbbreviatedGenusWithPeriod("G.")).to.be.true;
+    expect(isAbbreviatedGenusWithPeriod("Gr.")).to.be.true;
+  });
+  it('recognizes non-abbreviations', function() {
+    expect(isAbbreviatedGenusWithPeriod("Gr")).to.be.false;
+    expect(isAbbreviatedGenusWithPeriod("Gro")).to.be.false;
+  });
+});
+
+describe('#startsWithPunctuation', function() {
+  it('recognizes strings starting with punctuation', function() {
+    expect(startsWithPunctuation(".(Felis")).to.be.true;
+    expect(startsWithPunctuation(";Felis")).to.be.true;
+  });
+  it('recognizes strings not starting with punctuation', function() {
+    expect(startsWithPunctuation("Felis")).to.be.false;
+    expect(startsWithPunctuation("Felis;")).to.be.false;
+  });
+});
+
+describe('#endsWithPunctuation', function() {
+  it('recognizes strings ending with punctuation', function() {
+    expect(endsWithPunctuation("Felis)")).to.be.true;
+    expect(endsWithPunctuation("Felis;")).to.be.true;
+  });
+  it('recognizes strings not ending with punctuation', function() {
+    expect(endsWithPunctuation("Felis")).to.be.false;
+    expect(endsWithPunctuation(";Felis")).to.be.false;
+  });
+});
 
 describe('#checkWordAgainstState', function() {
   it('does nothing with null', function() {
     expect(checkWordAgainstState()).to.be.null;
   });
+  it('returns working name when finding potential abbreviations', function() {
+    var response = checkWordAgainstState('F.', { workingName: 'Felis', workingRank: 'genus' });
+    expect(response['workingName']).to.eq('F');
+    expect(response['workingRank']).to.eq('genus');
+    expect(response['returnNames'][0]).to.eq('Felis');
+  });
   it('recognizes potential abbreviated genera', function() {
-    var response = checkWordAgainstState('P.');
-    expect(response['workingName']).to.eq('P');
+    var response = checkWordAgainstState('F.');
+    expect(response['workingName']).to.eq('F');
     expect(response['workingRank']).to.eq('genus');
   });
   it('recognizes genera', function() {
-    var response = checkWordAgainstState('Pomatomus');
-    expect(response['workingName']).to.eq('Pomatomus');
+    var response = checkWordAgainstState('Felis');
+    expect(response['workingName']).to.eq('Felis');
     expect(response['workingRank']).to.eq('genus');
   });
   it('recognizes genera with full stops', function() {
-    var response = checkWordAgainstState('Pomatomus;');
-    expect(response['returnNames'][0]).to.eq('Pomatomus');
+    var response = checkWordAgainstState('Felis;');
+    expect(response['workingName']).to.be.undefined;
+    expect(response['returnNames'][0]).to.eq('Felis');
   });
   it('recognizes families and above', function() {
     var response = checkWordAgainstState('Animalia');
+    expect(response['workingName']).to.be.undefined;
     expect(response['returnNames'][0]).to.eq('Animalia');
   });
   it('does nothing with nonsense', function() {
     expect(checkWordAgainstState('nonsense')).to.be.null;
   });
-  it('will return the last known name when encountering nonsense', function() {
-    var response = checkWordAgainstState('nonsense', { workingName: 'Pomatomus' });
-    expect(response['returnNames'][0]).to.eq('Pomatomus');
+  it('returns the last known name when encountering nonsense', function() {
+    var response = checkWordAgainstState('nonsense', { workingName: 'Felis', workingRank: 'genus' });
+    expect(response['workingName']).to.be.undefined;
+    expect(response['returnNames'][0]).to.eq('Felis');
+  });
+  // State Genus
+  it('returns the species name if there is terminating punctuation', function() {
+    var response = checkWordAgainstState('leo;', { workingName: 'Felis', workingRank: 'genus' });
+    expect(response['workingName']).to.be.undefined;
+    expect(response['returnNames'][0]).to.eq('Felis leo');
+  });
+  it('attaches species to genera', function() {
+    var response = checkWordAgainstState('leo', { workingName: 'Felis', workingRank: 'genus' });
+    expect(response['workingName']).to.eq('Felis leo');
+    expect(response['workingRank']).to.eq('species');
+  });
+  // State Species
+  it('attaches species to species', function() {
+    var response = checkWordAgainstState('leo', { workingName: 'Felis leo', workingRank: 'species' });
+    expect(response['workingName']).to.eq('Felis leo leo');
+    expect(response['workingRank']).to.eq('species');
+  });
+  it('attaches ranks to species', function() {
+    var response = checkWordAgainstState('var', { workingName: 'Felis leo', workingRank: 'species' });
+    expect(response['workingName']).to.eq('Felis leo var');
+    expect(response['workingRank']).to.eq('rank');
+  });
+  it('attaches ranks to species and keeps punctuation', function() {
+    var response = checkWordAgainstState('var.', { workingName: 'Felis leo', workingRank: 'species' });
+    expect(response['workingName']).to.eq('Felis leo var.');
+    expect(response['workingRank']).to.eq('rank');
+  });
+  it('starts a new name when in species and found genus', function() {
+    var response = checkWordAgainstState('Amanita', { workingName: 'Felis leo', workingRank: 'species' });
+    expect(response['workingName']).to.eq('Amanita');
+    expect(response['workingRank']).to.eq('genus');
+    expect(response['returnNames'][0]).to.eq('Felis leo');
+  });
+  it('returns two names when in species and found family or above', function() {
+    var response = checkWordAgainstState('Animalia', { workingName: 'Felis leo', workingRank: 'species' });
+    expect(response['workingName']).to.be.undefined;
+    expect(response['returnNames'][0]).to.eq('Felis leo');
+    expect(response['returnNames'][1]).to.eq('Animalia');
+  });
+  it('returns species when in species and the next word is nonsense', function() {
+    var response = checkWordAgainstState('asdfasdf', { workingName: 'Felis leo', workingRank: 'species' });
+    expect(response['workingName']).to.be.undefined;
+    expect(response['returnNames'][0]).to.eq('Felis leo');
+  });
+  // State Rank
+  it('attaches species to ranks', function() {
+    var response = checkWordAgainstState('leo', { workingName: 'Felis leo var.', workingRank: 'rank' });
+    expect(response['workingName']).to.eq('Felis leo var. leo');
+    expect(response['workingRank']).to.eq('species');
+  });
+  it('starts a new name when in rank and found genus', function() {
+    var response = checkWordAgainstState('Amanita', { workingName: 'Felis leo var.', workingRank: 'rank' });
+    expect(response['workingName']).to.eq('Amanita');
+    expect(response['workingRank']).to.eq('genus');
+    expect(response['returnNames'][0]).to.eq('Felis leo var.');
+  });
+  it('returns two names when in rank and found family or above', function() {
+    var response = checkWordAgainstState('Animalia', { workingName: 'Felis leo var.', workingRank: 'rank' });
+    expect(response['workingName']).to.be.undefined;
+    expect(response['returnNames'][0]).to.eq('Felis leo var.');
+    expect(response['returnNames'][1]).to.eq('Animalia');
+  });
+  it('returns name when in rank and the next word is nonsense', function() {
+    var response = checkWordAgainstState('asdfasdf', { workingName: 'Felis leo var.', workingRank: 'rank' });
+    expect(response['workingName']).to.be.undefined;
+    expect(response['returnNames'][0]).to.eq('Felis leo var.');
   });
 });
 
@@ -67,7 +196,7 @@ describe('#prepareReturnString', function() {
   it('avoids infinite loops', function() {
     // I don't have a real example of this bug, but there are ways
     // to get stuck in a loop and this is a way to check for it.
-    // (in index.js => if(currentString == nextString) ...)
+    // (in index.js => if(currentString === nextString) ...)
     expect(prepareReturnString('Amanita [] muscaria')).to.eq('Amanita [] muscaria');
   });
   it('leaves everything else alone', function() {
@@ -78,88 +207,88 @@ describe('#prepareReturnString', function() {
 
 describe('#isSpecies', function() {
   it('fails on special characters', function() {
-    expect(isSpecies('(musculus', 'musculus', 'musculus', 'Mus')).to.be.null;
+    expect(isSpecies(buildState('(musculus', 'musculus', 'musculus', 'Mus'))).to.be.null;
   });
   it('fails on strings in the species_bad dictionary', function() {
-    expect(isSpecies('phobia', 'phobia', 'phobia', 'Mus')).to.be.null;
+    expect(isSpecies(buildState('phobia', 'phobia', 'phobia', 'Mus'))).to.be.null;
   });
   it('fails on strings with numbers in them', function() {
-    expect(isSpecies('phob1a', 'phob1a', 'phob1a', 'Mus')).to.be.null;
+    expect(isSpecies(buildState('phob1a', 'phob1a', 'phob1a', 'Mus'))).to.be.null;
   });
   it('fails on lowercase strings when genera are capitalized', function() {
-    expect(isSpecies('musculus', 'musculus', 'musculus', 'MUS')).to.be.null;
+    expect(isSpecies(buildState('musculus', 'musculus', 'musculus', 'MUS'))).to.be.null;
   });
   it('fails on capitalizes strings when genera are lower case', function() {
-    expect(isSpecies('MUSCULUS', 'MUSCULUS', 'musculus', 'Mus')).to.be.null;
+    expect(isSpecies(buildState('MUSCULUS', 'MUSCULUS', 'musculus', 'Mus'))).to.be.null;
   });
   it('fails on non-species strings', function() {
-    expect(isSpecies('nonsense', 'nonsense', 'nonsense', 'Mus')).to.be.null;
+    expect(isSpecies(buildState('nonsense', 'nonsense', 'nonsense', 'Mus'))).to.be.null;
   });
   it('returns 1 for valid species', function() {
-    expect(isSpecies('musculus', 'musculus', 'musculus', 'Mus')).to.eq(1);
+    expect(isSpecies(buildState('musculus', 'musculus', 'musculus', 'Mus'))).to.eq(1);
   });
   it('returns 1 for valid species when everything is capitalized', function() {
-    expect(isSpecies('MUSCULUS', 'MUSCULUS', 'musculus', 'MUS')).to.eq(1);
+    expect(isSpecies(buildState('MUSCULUS', 'MUSCULUS', 'musculus', 'MUS'))).to.eq(1);
   });
 });
 
 describe('#isNotGenusOrFamily', function() {
   it('true for short strings', function() {
-    expect(isNotGenusOrFamily('Aa', 'Aa', 'aa')).to.be.true;
+    expect(isNotGenusOrFamily(buildState('Aa', 'Aa', 'aa'))).to.be.true;
   });
   it('true for poorly capitalized strings', function() {
-    expect(isNotGenusOrFamily('amanita', 'amanita', 'amanita')).to.be.true;
-    expect(isNotGenusOrFamily('AMANita', 'AMANita', 'amanita')).to.be.true;
+    expect(isNotGenusOrFamily(buildState('amanita', 'amanita', 'amanita'))).to.be.true;
+    expect(isNotGenusOrFamily(buildState('AMANita', 'AMANita', 'amanita'))).to.be.true;
   });
   it('true for strings in the overlap dictionary', function() {
-    expect(isNotGenusOrFamily('Goliath', 'Goliath', 'goliath')).to.be.true;
+    expect(isNotGenusOrFamily(buildState('Goliath', 'Goliath', 'goliath'))).to.be.true;
   });
   it('false for everything else', function() {
-    expect(isNotGenusOrFamily('Amanita', 'Amanita', 'amanita')).to.be.false;
+    expect(isNotGenusOrFamily(buildState('Amanita', 'Amanita', 'amanita'))).to.be.false;
   });
 });
 
 describe('#isGenus', function() {
   it('fails on non-genera strings', function() {
-    expect(isGenus('AMANita', 'AMANita', 'amanita')).to.be.null;
+    expect(isGenus(buildState('AMANita', 'AMANita', 'amanita'))).to.be.null;
   });
   it('fails on non-genera strings', function() {
-    expect(isGenus('Itsnotaname', 'Itsnotaname', 'itsnotaname')).to.be.null;
+    expect(isGenus(buildState('Itsnotaname', 'Itsnotaname', 'itsnotaname'))).to.be.null;
   });
   it('returns 0.5 for ambiguous genera', function() {
-    expect(isGenus('Tuberosa', 'Tuberosa', 'tuberosa')).to.eq(0.5);
+    expect(isGenus(buildState('Tuberosa', 'Tuberosa', 'tuberosa'))).to.eq(0.5);
   });
   it('returns 1 for unambiguous genera', function() {
-    expect(isGenus('Amanita', 'Amanita', 'amanita')).to.eq(1);
+    expect(isGenus(buildState('Amanita', 'Amanita', 'amanita'))).to.eq(1);
   });
 });
 
 describe('#isFamilyOrAbove', function() {
   it('fails on non-genera strings', function() {
-    expect(isFamilyOrAbove('ANIMalia', 'ANIMalia', 'animalia')).to.be.null;
+    expect(isFamilyOrAbove(buildState('ANIMalia', 'ANIMalia', 'animalia'))).to.be.null;
   });
   it('fails on non-family-or-above strings', function() {
-    expect(isFamilyOrAbove('Itsnotaname', 'Itsnotaname', 'itsnotaname')).to.be.null;
+    expect(isFamilyOrAbove(buildState('Itsnotaname', 'Itsnotaname', 'itsnotaname'))).to.be.null;
   });
   it('returns 0.5 for ambiguous genera', function() {
-    expect(isFamilyOrAbove('Sorghum', 'Sorghum', 'sorghum')).to.eq(0.5);
+    expect(isFamilyOrAbove(buildState('Sorghum', 'Sorghum', 'sorghum'))).to.eq(0.5);
   });
   it('returns 1 for unambiguous genera', function() {
-    expect(isFamilyOrAbove('Animalia', 'Animalia', 'animalia')).to.eq(1);
+    expect(isFamilyOrAbove(buildState('Animalia', 'Animalia', 'animalia'))).to.eq(1);
   });
 });
 
 describe('#isRank', function() {
   it('fails on special characters', function() {
-    expect(isRank('(var', 'var', 'var')).to.be.null;
+    expect(isRank(buildState('(var', 'var', 'var'))).to.be.null;
   });
   it('fails on capital characters', function() {
-    expect(isRank('VAR', 'VAR', 'var')).to.be.null;
+    expect(isRank(buildState('VAR', 'VAR', 'var'))).to.be.null;
   });
   it('fails on non-ranks', function() {
-    expect(isRank('nonsense', 'nonsense', 'nonsens')).to.be.null;
+    expect(isRank(buildState('nonsense', 'nonsense', 'nonsens'))).to.be.null;
   });
   it('returns 1 for valid ranks', function() {
-    expect(isRank('var', 'var', 'var')).to.eq(1);
+    expect(isRank(buildState('var', 'var', 'var'))).to.eq(1);
   });
 });
